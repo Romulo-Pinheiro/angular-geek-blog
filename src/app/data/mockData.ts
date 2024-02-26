6fe4bfe4937885f71e6b3baf4e8da9d4f0ddb590
Romulo Pinheiro <romuloe.p.jesus@gmail.com>
2024-02-25 23:15:57 -0300

    Feat: Add Content Page and its Routing

---
+++ src/app/data/mockData.ts
@@ -0,0 +1,31 @@
+export const mockData = [
+  {
+    id: '1',
+    photoCover:
+      'https://img.olhardigital.com.br/wp-content/uploads/2023/07/Destaque-Poster-de-Oppenheimer.jpg',
+    title: 'Oppenheimer é indicado ao Oscar 2024',
+    description:
+      'O filme lidera com 13 indicações na maior premiação do cinema. A cerimônia ocorrerá no dia 10 de março.',
+  },
+  {
+    id: '2',
+    photoCover:
+      'https://cdn.akamai.steamstatic.com/steam/apps/1716740/capsule_616x353.jpg?t=1704299959',
+    title: 'Starfield promete novidades para 2024',
+    description: '',
+  },
+  {
+    id: '3',
+    photoCover:
+      'https://t.ctcdn.com.br/Fvptbj1dqz44LSHULZhyU9K-KIQ=/1024x576/smart/i525540.jpeg',
+    title: 'Série de Harry Potter tem previsão de lançamento para 2026',
+    description: '',
+  },
+  {
+    id: '4',
+    photoCover:
+      'https://cloud.estacaonerd.com/wp-content/uploads/2023/12/05100304/Bob-Marley.jpg',
+    title: 'One Love ultrapassa Madame Teia nas bilheterias',
+    description: '',
+  },
+];
